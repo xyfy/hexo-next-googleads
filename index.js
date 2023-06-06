c0c7be3fe56e2e17c456722d8ec734ed1e324a96
zhendong.dong <benguahao@foxmail.com>
2023-06-06 17:48:52 +0800

    fix: generate google ads.txt

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 const Util = require('@next-theme/utils');
 const utils = new Util(hexo, __dirname);
 const fs = require('hexo-fs');
+const path = require('path');
+
 hexo.extend.filter.register('theme_inject', injects => {
 
   let config = utils.defaultConfigFile('googleads', 'default.yaml');
@@ -14,18 +16,21 @@ hexo.extend.filter.register('theme_inject', injects => {
     hexo.log.warn(`googleads.client_id can't be null.`);
     return;
   }
-  if (!config.ads_txt) {
+  //console.debug(config.ads_txt);
+  if (config.ads_txt) {
     // add content to ads.txt, if not exist ,create it, otherwise, append it
     let rootDir = process.cwd();
     let adstxtPath = path.join(rootDir, 'source/ads.txt')
-    if (fs.exists(adstxtPath)) {
-      let content = fs.readFile(adstxtPath);
-      if (!content.includes(config.ads_txt)) {
-        fs.appendFile(config.ads_txt)
+    //console.debug(adstxtPath);
+    if (fs.existsSync(adstxtPath)) {
+      let content = fs.readFileSync(adstxtPath, { encoding: 'utf-8' });
+      //console.debug(content);
+      if (content.indexOf(config.ads_txt) == -1) {
+        fs.appendFileSync(adstxtPath, '\n' + config.ads_txt);
       }
     }
     else {
-      fs.writeFile(adstxtPath, config.ads_txt);
+      fs.writeFileSync(adstxtPath, config.ads_txt);
     }
   }
   injects.head.raw('googleads', '<script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + config.client_id + '" crossorigin="anonymous"></script>');
